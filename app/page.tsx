c5cf96d421d72ad2d6ee58864be4b85a28350518
dieftsx <diefersonfrontenddeveloper@gmail.com>
2026-04-01 22:05:22 -0400

    Feat: add functions in home

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,20 @@ import Image from "next/image";
 import { redirect } from '@/lib/supabase/server'
 
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient()
+  const { data:  {user}, error } = await supabase.auth.getUser()
+  
+  if (error || !user) {
+    redirect('/auth/login')
+  }
 
+
+  //Busca dados da empresa do usuário
+  const { data: empresa } = await supabase
+  .from ('empresas')
+  .select('*')
+  .eq('user', user.id)
+  .single()
+  return <ChatContainer user={user} empresa={empresa} />
 }
