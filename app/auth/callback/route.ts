b14da9cb612a0eef63e99840214ca42dfbbf459c
dieftsx <diefersonfrontenddeveloper@gmail.com>
2026-05-12 19:00:31 -0400

    HotFix: Logic in Callback route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -29,9 +29,7 @@ export async function GET(request:Request) {
           catch (error) {
             console.log(error)
           }
-        }
-      }
-    )
+        )
     const { error } = await supabase.auth.exchangeCodeForSession(code)
 
     if(!error){
